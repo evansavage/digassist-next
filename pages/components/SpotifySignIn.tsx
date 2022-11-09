55d7afda993958cfafa3e2f3d007b569743a6cc7
evansavage <esav678@gmail.com>
2022-11-09 17:30:17 -0500

    Package updates

--- pages/components/SpotifySignIn.tsx
+++ pages/components/SpotifySignIn.tsx
@@ -1,7 +1,4 @@
 import React, { useEffect } from "react";
-// import GoogleLogin from "react-google-login";
-import { useGoogleLogin } from "@react-oauth/google";
-import axios from "axios";
 
 const SPOTIFY_CLIENT_ID = "e7d2ff66c7054a389f0c5c65db30bf46";
 const REDIRECT_URI = "http://localhost:3000";
